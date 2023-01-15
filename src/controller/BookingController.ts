f9afdc766a20daed689022bda4bf8a02554b48fb
polieftov <polieftovegor@gmail.com>
2023-01-15 20:28:18 +0500

    роут для получения информации о "свободности" секции для бронирования

--- src/controller/BookingController.ts
+++ src/controller/BookingController.ts
@@ -1,16 +1,53 @@
-import {Body, Delete, Get, HttpCode, JsonController, OnUndefined, Param, Post, UseAfter} from 'routing-controllers'
+import {
+    Body,
+    Delete,
+    Get,
+    HttpCode,
+    JsonController,
+    OnUndefined,
+    Param,
+    Post,
+    QueryParam,
+    UseAfter
+} from 'routing-controllers'
 import 'reflect-metadata'
 import log4js from "log4js";
 import {CovorcSection} from "../models/CovorcSection.js";
 import {Booking} from "../models/Booking.js";
 import {User} from "../models/User.js";
 import {loggingMiddleware} from "./Middleware/Auth.js";
+import {sequelize} from "../models/dbconnection.js";
+import {QueryTypes} from "sequelize";
 
 const logger = log4js.getLogger()
 
 @JsonController()
 @UseAfter(loggingMiddleware)
 export class BookingController {
+    @Get('/bookings/free')
+    async getBookingsByCovorcSectionsAndDatePeriod(
+        @QueryParam("covorcSection") covorcSectionId: number,
+        @QueryParam("beginDate") beginDate: Date,
+        @QueryParam("endDate") endDate: Date
+    ): Promise<BookingsByHour[]> {
+        const query = `select bbh."date", to_char(bbh."date", 'dd') "day", to_char(bbh."date", 'hh24') "hour", 
+         sum(b."countOfPeople") countOfPeople, max(cs."placesCount") as placesCount,
+         max(cs."placesCount") - sum(b."countOfPeople") avaiblePlaces
+         from "bookingByHours" bbh
+         join bookings b on b.id = bbh."bookingId"
+         join covorc_sections cs on cs.id = b."covorcSectionId"
+         where cs.id = ${covorcSectionId} and bbh."date" between ${beginDate} and ${endDate} 
+         group by "day", "hour", bbh."date"
+         order by "day", "hour", bbh."date"`
+
+        return sequelize.query(
+            query,
+            {
+                type: QueryTypes.SELECT
+            }
+        )
+    }
+
     @Get('/bookings/:id')
     @HttpCode(200)
     @OnUndefined(400)
@@ -26,6 +63,8 @@ export class BookingController {
         logger.debug(`get all bookings`);
         return JSON.stringify(await Booking.findAll({include: [CovorcSection, User]}));
     }
+
+
 
     /**
      * {
@@ -58,4 +97,13 @@ export class BookingController {
         });
         logger.debug(`booking with id = ${id} was deleted`);
     }
+}
+
+type BookingsByHour = {
+    date: Date
+    day: number
+    hour: number
+    countOfPeople: number
+    placesCount: number
+    availablePlacesCount: number
 }
